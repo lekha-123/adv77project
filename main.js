cb287dcd30c5eac54a1d67541ed5c11618b83603
lekha-123 <71594197+lekha-123@users.noreply.github.com>
2021-04-10 18:37:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var word1="hello"
+var word2="this is my first console project"
+console.log(word1);
+console.log(word2);
+var join= word1.concat(word2)
+console.log(join);
+var number1= 65
+var number2=5
+console.log(number1);
+console.log(number2);
+console.log(number1+number2);
+console.log(number1-number2);
+console.log(number1*number2);
+console.log(number1/number2);
